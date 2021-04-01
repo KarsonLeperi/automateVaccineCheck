3cb56398fd19efbe711782d984e362151e4a6956
KarsonLeperi <karsonleperi@gmail.com>
2021-04-01 10:47:19 -0700

    Create runFile.py

---
+++ runFile.py
@@ -0,0 +1,6 @@
+
+#Load Files from Secret file (initalization parameters)
+
+#set up while loop. runs python file for costco check and vaxxmax check. breaks when it finds a solution
+
+#sends text to me to alert about finding
