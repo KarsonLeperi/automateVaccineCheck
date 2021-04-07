f1caa30dba38932251db0145bb71b70d5901aceb
KarsonLeperi <karsonleperi@gmail.com>
2021-04-07 09:26:22 -0700

    Update secret.py
    Added descriptions

--- secret.py
+++ secret.py
@@ -1,33 +1,35 @@
 class PersonalData:
 	"This is the personal data"
 	webdriver= "Test"
-	sleeptime = 60
+	sleeptime = 60     # This is the time between different checks of the website. The website only refreshes every minute so the minimum should be 60
 	max_distance = 150
 	state = "OR"
 	notification = "email"   # Choose text, email, or none
-	max_runtime = 300
+	max_runtime = 300     # max run time for the program to run and attempt to find a slot
 	urls = ['http://vaxxmax.com/cvs',
         'http://vaxxmax.com/walgreens',
         'http://vaxxmax.com/riteaid',
 		'http://vaxxmax.com/walmart']
 
 	#Email Settings
-	email_acct = 
-	email_pwd =
-	email_recepient = 
+	#You might need to allow less secure apps to work https://myaccount.google.com/lesssecureapps
+	email_acct =          # email address to use (recommend to use gmail, otherwise you will need to update the smtp_server variable)
+	email_pwd =	      # password for email address above
+	email_recepient =     # email to send the alert to (can use the same email as email_acct
 	email_subject = "Alert"
 	smtp_server = "smtp.gmail.com"
 	smtp_port = 587
 	
 	#Text Message Settings
+	# for this feature to work, you will need to create an account on twilio. They will provide you with the account_sid, auth_token, and message_from (phone number) variable.
 	from twilio.rest import Client
 	
 
-	account_sid = 
-	auth_token = 
+	account_sid =       # get from Twilio
+	auth_token =        # Get from Twilio
 	client = Client(account_sid, auth_token)
-	message_from = 
-	message_to=
+	message_from =      # Get from Twilio
+	message_to=	    # Personal phone number
 
 
 	
